cd7b819c68df2eb5e9de93430d23793c27a7f186
Filipe Lima <fhemrique55@gmail.com>
2024-07-22 03:40:21 -0300

    feat: activate, desactivate and delete boarding passes

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,20 +11,33 @@ export class UserService {
 
   constructor(private readonly _http: HttpClient) { }
 
-  findById (id: number): Observable<IUser> {
+  findById(id: number): Observable<IUser> {
     return this._http.get<IUser>(`${API_CONFIG.baseUrl}/api/users/${id}`);
   }
 
-  findAll(): Observable<IUser[]>{
+  findAll(): Observable<IUser[]> {
     return this._http.get<IUser[]>(`${API_CONFIG.baseUrl}/api/users`)
   }
 
-  create(user: IUser): Observable<IUser[]>{
+  create(user: IUser): Observable<IUser[]> {
     return this._http.post<IUser[]>(`${API_CONFIG.baseUrl}/api/users`, user)
   }
 
-  update(user: IUser): Observable<IUser>{
+  update(user: IUser): Observable<IUser> {
     return this._http.put<IUser>(`${API_CONFIG.baseUrl}/api/users/${user.id}`, user)
+  }
+
+  deleteBoardingPass(userId: number, boardingPassId: number): Observable<void> {
+    const url = `${API_CONFIG.baseUrl}/api/users/${userId}/boarding-passes/${boardingPassId}`;
+    return this._http.delete<void>(url);
+  }
+
+  activateBoardingPass(userId: number, boardingPassId: number): Observable<void> {
+    return this._http.put<void>(`${API_CONFIG.baseUrl}/api/users/${userId}/${boardingPassId}/activate`, {});
+  }
+
+  deactivateBoardingPass(userId: number, boardingPassId: number): Observable<void> {
+    return this._http.delete<void>(`${API_CONFIG.baseUrl}/api/users/${userId}/${boardingPassId}/deactivate`, {});
   }
 
 }
